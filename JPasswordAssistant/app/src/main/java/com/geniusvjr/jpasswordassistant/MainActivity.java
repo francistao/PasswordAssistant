3656fe420b92aa02ffce6e425be14b8d74a09ad3
geniusvjr <geniusvjr@gmail.com>
2016-05-28 11:32:15 +0800

    5.28提交
    实现了Activity基类的第二层封装，实现了沉浸式状态栏，以及一系列框架的注册和反注册。以及设置主题的，获取不同主题的颜色属性来改变状态栏的颜
    色等。

--- JPasswordAssistant/app/src/main/java/com/geniusvjr/jpasswordassistant/MainActivity.java
+++ JPasswordAssistant/app/src/main/java/com/geniusvjr/jpasswordassistant/MainActivity.java
@@ -2,12 +2,57 @@ package com.geniusvjr.jpasswordassistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.BaseAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import com.geniusvjr.jpasswordassistant.mvp.model.eventbus.EventCenter;
+import com.geniusvjr.jpasswordassistant.mvp.ui.activity.base.Base;
+import com.geniusvjr.jpasswordassistant.mvp.ui.activity.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected TransitionMode getOverridePendingTransitionMode() {
+        return null;
+    }
+
+    @Override
+    protected boolean toggleOverridePendingTransition() {
+        return false;
+    }
+
+    @Override
+    protected void onEventComing(EventCenter eventCenter) {
+
+    }
+
+    @Override
+    protected int getContentView() {
+        return 0;
+    }
+
+    @Override
+    protected void initToolbar() {
+
+    }
+
+    @Override
+    protected boolean isApplyTranslucency() {
+        return false;
+    }
+
+    @Override
+    protected boolean isApplyButterKnife() {
+        return false;
+    }
+
+    @Override
+    protected boolean isApplyEventBus() {
+        return false;
     }
 }
